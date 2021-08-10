8d65e7ea89cebe16812957bd343e881c2c5be97b
tsadkan <xad@tsadkans-MacBook-Pro.local>
2021-08-10 13:22:03 -0500

    basic setup with registry and gateway

---
+++ src/main/java/cusomerservice/customerservice/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package productservice.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+    @GetMapping("/customer")
+    public String defaultMethod() {
+        return "customer service";
+    }
+}
